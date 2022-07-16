2b6246286864e0b6fcf63ec7bd55bcca82c736ca
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-07-16 14:47:20 +0545

    start accounts app
    write first unit test,
    create ImpersonalAccount model

---
+++ accounts/tests.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from accounts.models import ImpersonalAccount
+
+# Create your tests here.
+
+class AccountModelTest(TestCase):
+
+    def test_create_and_retreive_accounts(self):
+        parent_ac = ImpersonalAccount.objects.create(
+                name='Kosh',
+                type_ac='LI',
+                code='20',
+        )
+        child_ac = ImpersonalAccount.objects.create(
+                name='Jageda kosh',
+                parent_ac=parent_ac,
+                type_ac='LI',
+                code='20.1',
+        )
+
+        saved_accounts = ImpersonalAccount.objects.all()
+
+        self.assertEqual(saved_accounts.count(), 2)
+        self.assertEqual(saved_accounts[0].name, 'Kosh')
+        self.assertEqual(saved_accounts[0].parent_ac, None)
+        self.assertEqual(saved_accounts[0].type_ac, 'LI')
+        self.assertEqual(saved_accounts[0].code, '20')
+
+        self.assertEqual(saved_accounts[1].name, 'Jageda kosh')
+        self.assertEqual(saved_accounts[1].parent_ac, parent_ac)
+        self.assertEqual(saved_accounts[1].type_ac, 'LI')
+        self.assertEqual(saved_accounts[1].code, '20.1')
