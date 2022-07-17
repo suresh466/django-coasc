c1c8f509b1be046606389ed63d81937265a77ceb
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-07-17 18:45:41 +0545

    create helper function for journals.tests
    change code layout

--- journals/tests.py
+++ journals/tests.py
@@ -1,35 +1,32 @@
 from django.test import TestCase
+
 from accounts.models import ImpersonalAccount
 from journals.models import Split, Transaction
 
 # Create your tests here.
 
+
 class TransactionAndSplitModelTest(TestCase):
 
+    def create_impersonal_account(self, name, type_ac, code, parent_ac=None):
+        return ImpersonalAccount.objects.create(
+                name=name, code=code, type_ac=type_ac, parent_ac=parent_ac)
+
+    def create_transaction(self, description=''):
+        return Transaction.objects.create(description=description)
+
+    def create_split(self, transaction, account, type_split, amount):
+        return Split.objects.create(
+                transaction=transaction, account=account,
+                type_split=type_split, amount=amount)
+
     def test_create_and_retreive_splits(self):
-        transaction = Transaction.objects.create()
-        ac_1 = ImpersonalAccount.objects.create(
-            name='Share pujji',
-            type_ac='LI',
-            code='10',
-        )
-        ac_2 = ImpersonalAccount.objects.create(
-            name='Nagad hissab',
-            type_ac='AS',
-            code='80',
-        )
-        Split.objects.create(
-            transaction=transaction,
-            account=ac_2,
-            type_split='dr',
-            amount=15500,
-        )
-        Split.objects.create(
-            transaction=transaction,
-            account=ac_1,
-            type_split='cr',
-            amount=15500,
-        )
+        transaction = self.create_transaction(description='')
+        ac_1 = self.create_impersonal_account('Share pujji', 'LI', '10')
+        ac_2 = self.create_impersonal_account('Nagad hissab', 'AS', '80')
+
+        self.create_split(transaction, ac_2, 'dr', 15500)
+        self.create_split(transaction, ac_1, 'cr', 15500)
 
         saved_split = Split.objects.all()
 
@@ -43,51 +40,42 @@ class TransactionAndSplitModelTest(TestCase):
         self.assertEqual(saved_split[1].amount, 15500)
 
     def test_create_and_retreive_transactions(self):
-        Transaction.objects.create(description='first description')
-        Transaction.objects.create(description='')
-        Transaction.objects.create()
+        self.create_transaction('first description')
+        self.create_transaction('')
+        self.create_transaction()
 
         saved_transactions = Transaction.objects.all()
 
         self.assertEqual(saved_transactions.count(), 3)
-        self.assertEqual(saved_transactions[0].description, 'first description')
+        self.assertEqual(
+                saved_transactions[0].description, 'first description')
         self.assertEqual(saved_transactions[1].description, '')
         self.assertEqual(saved_transactions[2].description, '')
 
     def test_create_and_retreive_transaction_with_splits(self):
-        ac_1 = ImpersonalAccount.objects.create(
-            name='Share pujji',
-            type_ac='LI',
-            code='10',
-        )
-        ac_2 = ImpersonalAccount.objects.create(
-            name='Nagad hissab',
-            type_ac='AS',
-            code='80',
-        )
+        ac_1 = self.create_impersonal_account('Share pujji', 'LI', '10')
+        ac_2 = self.create_impersonal_account('Nagad hissab', 'AS', '80')
         transaction = Transaction.objects.create(
-            description='first description'
-        )
-        split_1 = Split.objects.create(
-            transaction=transaction,
-            account=ac_2,
-            type_split='dr',
-            amount=15500,
-        )
-        split_2 = Split.objects.create(
-            transaction=transaction,
-            account=ac_1,
-            type_split='cr',
-            amount=15500,
-        )
+                description='first description')
+
+        split_1 = self.create_split(
+                transaction=transaction, account=ac_2, type_split='dr',
+                amount=15500)
+        split_2 = self.create_split(
+                transaction=transaction, account=ac_1, type_split='cr',
+                amount=15500)
 
         saved_transaction = Transaction.objects.all()
         split_sets = saved_transaction[0].split_set.all()
 
         self.assertEqual(saved_transaction.count(), 1)
         self.assertEqual(
-            saved_transaction[0].description,
-            'first description',
-        )
+                saved_transaction[0].description, 'first description')
         self.assertEqual(split_sets[0], split_1)
         self.assertEqual(split_sets[1], split_2)
+
+    def test_transaction_not_saved_if_dr_cr_not_balanced(self):
+        pass
+
+    def test_transaction_not_saved_unless_dr_cr_is_balanced(self):
+        pass
