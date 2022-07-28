89914bbc2f747b6f184b9ffaaaab8b3b090b5c4e
Suresh Thagunna <suresh.thagunna77@gmail.com>
2022-07-28 10:11:12 +0545

    rename the project from coas to coas_core

--- coas_core/wsgi.py
+++ coas_core/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for coas project.
+WSGI config for coas_core project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coas.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coas_core.settings')
 
 application = get_wsgi_application()
